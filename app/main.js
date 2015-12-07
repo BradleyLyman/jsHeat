bdafacc02b9920ace2bf461dcc3c8625317ab46c
brad <lyman.brad3211@gmail.com>
2015-12-07 15:14:13 -0500

    created the sfRenderer

--- app/main.js
+++ app/main.js
@@ -9,55 +9,26 @@
       Router      = ReactRouter.Router,
       Route       = ReactRouter.Route,
       Link        = ReactRouter.Link,
-      ScalarField = require('./scalarField.js');
+      ScalarField = require('./scalarField.js'),
+      SfRenderer  = require('./sfRenderer.js');
 
   const Scene = React.createClass({
     mixins : [ThreeMixin],
 
     componentDidMount : function() {
-      if (this.scene === undefined) {
-        this.scene = new Three.Scene();
-
-        let field = ScalarField.create(8, 1.0);
-        field.set(2, 2, 0.0);
-        let dataTexture = field.asTexture();
-        let dataMaterial = new Three.MeshBasicMaterial({map: dataTexture});
-
-        let planeGeometry = new Three.PlaneGeometry(2, 2);
-        let material = new Three.ShaderMaterial({
-          uniforms : { data: { type: "t", value : dataTexture } },
-          vertexShader: `
-            varying vec2 varyUv;
-            void main() {
-              varyUv = uv;
-              gl_Position = modelViewMatrix * projectionMatrix * vec4(position, 1.0);
-            }
-          `,
-          fragmentShader: `
-            varying vec2 varyUv;
-
-            uniform sampler2D data;
-
-            void main() {
-              gl_FragColor = vec4(texture2D(data, varyUv).a, 0.0, 0.0, 1.0);
-            }
-          `,
-        });
-        this.cube = new Three.Mesh(planeGeometry, material);
-
-        this.scene.add(this.cube);
-      }
-
-      if (this.camera === undefined) {
-        this.camera = new Three.OrthographicCamera(
-          -1.0, 1.0, 1.0, -1.0, 1.0, -1.0
-        );
+      if (this.sfr === undefined) {
+        this.sfr = SfRenderer.create();
+        this.field = ScalarField.create(8, 1.0);
+        this.field.set(2, 3, 0.0);
+        this.field.set(5, 5, 0.0);
       }
       this.start();
     },
 
     updateScene : function() {
-      this.renderer.render(this.scene, this.camera);
+      this.sfr.render(
+        this.renderer, this.field.asTexture()
+      );
     },
 
     onResize : function() {
@@ -65,30 +36,17 @@
         width  : this.canvasWidth,
         height : this.canvasHeight,
       };
-      let aspect = dims.width/dims.height;
-      if (dims.width >= dims.height) {
-        this.camera.top = 1.0;
-        this.camera.bottom = -1.0;
-        this.camera.left = -aspect;
-        this.camera.right = aspect;
-      } else {
-        this.camera.left = -1.0;
-        this.camera.right = 1.0;
-        this.camera.top = 1.0/aspect;
-        this.camera.bottom = -1.0/aspect;
-      }
-      this.camera.updateProjectionMatrix();
+      this.sfr.resize(dims);
     },
 
     render : function() {
       return (
-        <div style={{ border : '1px solid black' }}>
-          <p>Scene</p>
+        <div>
           <canvas
             style={{ minWidth : '100%',
                      maxWidth : '100%',
-                     minHeight : '50vh',
-                     maxHeight : '51vh'}}
+                     minHeight : '90vh',
+                     maxHeight : '91vh'}}
             ref="canvas" />
         </div>
       );
---
+++ app/sfRenderer.js
@@ -0,0 +1,73 @@
+'use strict';
+let Three = require('three');
+
+const vertexShaderSrc = `
+  varying vec2 varyUv;
+  void main() {
+    varyUv = uv;
+    gl_Position =
+        modelViewMatrix * projectionMatrix *
+        vec4(position, 1.0);
+  }
+`;
+
+const fragmentShaderSrc = `
+  varying vec2 varyUv;
+  uniform sampler2D data;
+  void main() {
+    gl_FragColor =
+      texture2D(data, varyUv).a *
+      vec4(0.2, 0.2, 0.8, 1.0);
+  }
+`;
+
+let sfRenderer = {};
+
+sfRenderer.prototype = {};
+sfRenderer.prototype.render = function(renderer, dataTex) {
+  this.uniforms.data.value = dataTex;
+  renderer.render(this.scene, this.camera);
+};
+
+sfRenderer.prototype.resize = function(dims) {
+  let aspect = dims.width/dims.height;
+  if (dims.width >= dims.height) {
+    this.camera.top = 1.0;
+    this.camera.bottom = -1.0;
+    this.camera.left = -aspect;
+    this.camera.right = aspect;
+  } else {
+    this.camera.left = -1.0;
+    this.camera.right = 1.0;
+    this.camera.top = 1.0/aspect;
+    this.camera.bottom = -1.0/aspect;
+  }
+  this.camera.updateProjectionMatrix();
+};
+
+let createSfRenderer = function() {
+  let uniforms = { data : { type : "t" } };
+  let material = new Three.ShaderMaterial({
+    vertexShader   : vertexShaderSrc,
+    fragmentShader : fragmentShaderSrc,
+    uniforms       : uniforms,
+  });
+
+  let camera = new Three.OrthographicCamera(
+    -1.0, 1.0, 1.0, -1.0, 1.0, -1.0
+  );
+
+  let scene         = new Three.Scene();
+  let planeGeometry = new Three.PlaneGeometry(2, 2);
+  let plane = new Three.Mesh(planeGeometry, material);
+  scene.add(plane);
+  console.log(plane);
+  return {
+    __proto__ : sfRenderer.prototype,
+    scene     : scene,
+    camera    : camera,
+    uniforms  : uniforms,
+  };
+};
+
+module.exports.create = createSfRenderer;
